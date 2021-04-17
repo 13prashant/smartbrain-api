e3f9e4a0fe99e13c7371fcf56c9fa085a21db1ac
Prashant Patel <pacific1392@gmail.com>
2021-04-17 10:48:03 +0530

    git commit -m deploy git commit -m

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require('express')
 const cors = require('cors')
 const bcrypt = require('bcrypt')
 const knex = require('knex');
-const e = require('express');
 
 const signin = require('./controllers/signin')
 const register = require('./controllers/register')
@@ -21,20 +20,12 @@ const db = knex({
     }
 });
 
-// db.select('*').from('users')
-//     .then(data => {
-//         console.log(data)
-//     })
-
 const app = express()
 
 app.use(express.json())
 app.use(cors())
 
-// app.get('/', (req, res) => {
-//     res.json(database.users)
-// })
-
+app.get('/', (req, res) => res.json('It is working!'))
 app.post('/signin', (req, res) => signin.handleSignIn(req, res, db, bcrypt))
 app.post('/register', (req, res) => register.handleRegister(req, res, db, bcrypt, saltRounds))
 app.get('/profile/:id', (req, res) => profile.handleProfile(req, res, db))
